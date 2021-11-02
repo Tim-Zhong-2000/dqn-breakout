393ef68cf952c191102efcd558ee2e33d36efcfb
Tim-Zhong-2000 <fengyu.fyxdfg@gmail.com>
2021-11-02 21:36:25 +0800

    尝试修改网络结构，使用两层Linear层

--- utils_model.py
+++ utils_model.py
@@ -7,11 +7,18 @@ class DQN(nn.Module):
 
     def __init__(self, action_dim, device):
         super(DQN, self).__init__()
+        self.__dim = action_dim
         self.__conv1 = nn.Conv2d(4, 32, kernel_size=8, stride=4, bias=False)
         self.__conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2, bias=False)
         self.__conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1, bias=False)
-        self.__fc1 = nn.Linear(64*7*7, 512)
-        self.__fc2 = nn.Linear(512, action_dim)
+
+        self.__fc1_1 = nn.Linear(64*7*7, 512)
+        self.__fc1_2 = nn.Linear(64*7*7, 512)
+
+        self.__fc2_1 = nn.Linear(512, action_dim)
+        self.__fc2_2 = nn.Linear(512, 1)
+
+        self.__relu = nn.ReLU()
         self.__device = device
 
     def forward(self, x):
@@ -19,8 +26,13 @@ class DQN(nn.Module):
         x = F.relu(self.__conv1(x))
         x = F.relu(self.__conv2(x))
         x = F.relu(self.__conv3(x))
-        x = F.relu(self.__fc1(x.view(x.size(0), -1)))
-        return self.__fc2(x)
+        x = x.view(x.size(0), -1)
+        adv = self.__relu(self.__fc1_1(x))
+        val = self.__relu(self.__fc1_2(x))
+        adv = self.__fc2_1(adv)
+        val = self.__fc2_2(val).expand(x.size(0), self.__dim)
+        x = val + adv - adv.mean(1).unsqueeze(1).expand(x.size(0), self.__dim)
+        return x
 
     @staticmethod
     def init_weights(module):
